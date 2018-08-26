776b9ea21bf30da11078ef4e8f1d037a0496bb9c
Worawit Werapan <werapan@gmail.com>
2018-08-26 12:24:35 +0700

    Test Git

--- tests/OXTest.java
+++ tests/OXTest.java
@@ -130,4 +130,15 @@ class OXTest {
         assertTrue(ox.checkWin(1, 1));
         assertTrue(ox.checkWin(2, 2));
     }
+    @Test
+    void checkWin8() {
+        OX ox = new OX();
+        ox.put(0,0);
+        ox.put(1,1);
+        ox.put(2,2);
+        assertEquals(" 012\n0X--\n1-X-\n2--X\n",ox.getTableString());
+        assertTrue(ox.checkWin(0, 0));
+        assertTrue(ox.checkWin(1, 1));
+        assertTrue(ox.checkWin(2, 2));
+    }
 }
